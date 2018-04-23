8cbde9754ea6589b3d28bdb1e3c0b3622525abee
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+
+public partial class _Default : System.Web.UI.Page {
+    Session session = XpoHelper.GetNewSession();
+
+    protected void cmb_ItemRequestedByValue(object source, DevExpress.Web.ASPxEditors.ListEditItemRequestedByValueEventArgs e) {
+        MyObject obj = session.GetObjectByKey<MyObject>(e.Value);
+
+        if (obj != null) {
+            cmb.DataSource = new MyObject[] { obj };
+            cmb.DataBindItems();
+        }
+    }
+
+    protected void cmb_ItemsRequestedByFilterCondition(object source, DevExpress.Web.ASPxEditors.ListEditItemsRequestedByFilterConditionEventArgs e) {
+        XPCollection<MyObject> collection = new XPCollection<MyObject>(session);
+        collection.SkipReturnedObjects = e.BeginIndex;
+        collection.TopReturnedObjects = e.EndIndex - e.BeginIndex + 1;
+        collection.Criteria = new BinaryOperator("Title", String.Format("%{0}%", e.Filter), BinaryOperatorType.Like);
+        collection.Sorting.Add(new SortProperty("Oid", DevExpress.Xpo.DB.SortingDirection.Ascending));
+
+        cmb.DataSource = collection;
+        cmb.DataBindItems();
+    }
+}
---
+++ VB/WebSite/App_Code/XpoHelper.vb
@@ -0,0 +1,69 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Xpo
+Imports DevExpress.Xpo.DB
+Imports DevExpress.Xpo.Metadata
+
+''' <summary>
+''' Summary description for XpoHelper
+''' </summary>
+Public NotInheritable Class XpoHelper
+	Private Sub New()
+	End Sub
+	Shared Sub New()
+		CreateDefaultObjects()
+	End Sub
+
+	Public Shared Function GetNewSession() As Session
+		Return New Session(DataLayer)
+	End Function
+
+	Public Shared Function GetNewUnitOfWork() As UnitOfWork
+		Return New UnitOfWork(DataLayer)
+	End Function
+
+	Private ReadOnly Shared lockObject As Object = New Object()
+
+	Private Shared fDataLayer As IDataLayer
+	Private Shared ReadOnly Property DataLayer() As IDataLayer
+		Get
+			If fDataLayer Is Nothing Then
+				SyncLock lockObject
+					fDataLayer = GetDataLayer()
+				End SyncLock
+			End If
+			Return fDataLayer
+		End Get
+	End Property
+
+	Private Shared Function GetDataLayer() As IDataLayer
+		XpoDefault.Session = Nothing
+
+		Dim ds As New InMemoryDataStore()
+		Dim dict As XPDictionary = New ReflectionDictionary()
+		dict.GetDataStoreSchema(GetType(MyObject).Assembly)
+
+		Return New ThreadSafeDataLayer(dict, ds)
+	End Function
+
+	Private Shared Sub CreateDefaultObjects()
+		Using uow As UnitOfWork = GetNewUnitOfWork()
+			Dim obj As MyObject = Nothing
+
+			For i As Int32 = 0 To 122
+				obj = New MyObject(uow)
+				obj.Title = "Item " & i.ToString()
+			Next i
+
+			uow.CommitChanges()
+		End Using
+	End Sub
+End Class
